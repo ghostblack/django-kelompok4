82697ead37ed839d5b08189e29c54db93b7176cd
Adhikna Enggarjati EW <enggar308@gmail.com>
2021-11-26 14:25:02 +0700

    update settings.py
    menambahkan konfigurasi database monggo db

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Flask',
+        'CLIENT': {
+           'host': 'mongodb+srv://ghostblack:kfbLcKqWxdmYRnMr@flask.qc7wx.mongodb.net/Flask?retryWrites=true&w=majority',
+        }
     }
 }
 
